f85d7832e4347b6fd1901a41fb9531e7d6e46938
Glen Wider <glen.wider@pulsundzeit.de>
2024-05-06 19:48:14 +0200

    add toggle and refactor base methods

--- juice.php
+++ juice.php
@@ -6,10 +6,12 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class UpCommand extends Command
 {
-    protected static $defaultName = 'app:up';
+    protected static $defaultName = 'up';
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -20,19 +22,44 @@ class UpCommand extends Command
         }
 
         exec('docker run -d -p 3000:3000 bkimminich/juice-shop', $outputLines, $returnVar);
-        $output->writeln($outputLines);
+        $output->writeln("🧃🦕 slurp juice");
+
         return Command::SUCCESS;
     }
 }
 
 class DownCommand extends Command
 {
-    protected static $defaultName = 'app:down';
+    protected static $defaultName = 'down';
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         exec('docker stop $(docker ps -q --filter ancestor=bkimminich/juice-shop)', $outputLines, $returnVar);
-        $output->writeln($outputLines);
+        $output->writeln("juice-shop stopped 💤");
+
+        return Command::SUCCESS;
+    }
+}
+
+class ToggleCommand extends Command
+{
+    protected static $defaultName = 'toggle';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Check if the application is running
+        exec('docker ps -q --filter ancestor=bkimminich/juice-shop', $outputLines, $returnVar);
+
+        if (empty($outputLines)) {
+            // If the application is not running, start it
+            $upCommand = new UpCommand();
+            $upCommand->execute($input, $output);
+        } else {
+            // If the application is running, stop it
+            $downCommand = new DownCommand();
+            $downCommand->execute($input, $output);
+        }
+
         return Command::SUCCESS;
     }
 }
@@ -40,4 +67,17 @@ class DownCommand extends Command
 $app = new Application();
 $app->add(new UpCommand());
 $app->add(new DownCommand());
-$app->run();
+$app->add(new ToggleCommand());  
+
+# Check if any arguments were passed
+if ($argc == 1) {
+    # If no arguments were passed, default to the 'toggle' command
+    $input = new ArrayInput([
+        'command' => 'toggle',
+    ]);
+} else {
+    # If arguments were passed, use them as normal
+    $input = new ArgvInput();
+}
+
+$app->run($input);
